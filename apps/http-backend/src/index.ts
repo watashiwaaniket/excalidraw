c4ea02e7316837fe62faea9b062193aaba3f45d2
watashiwaaniket <aniketdhakane3@gmail.com>
2025-10-14 00:21:18 +0530

    setup boilerplate code

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello there") 
+import express, { json } from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({
+        "message" : "Goober nigger"
+    })
+})
+
+app.listen(3001, () => {
+    console.log("App listening on port 3001")
+})
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Ws server")
+import WebSocket, { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({
+    port: 8080
+})
+
+wss.on('connection', function connection(ws){
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data){
+        ws.send('pong');
+    })
+})
