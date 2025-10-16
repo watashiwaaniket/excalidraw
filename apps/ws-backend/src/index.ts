9762854fc5cbb645175e5a4ecaf2899c7d05d1e9
watashiwaaniket <aniketdhakane3@gmail.com>
2025-10-16 23:59:08 +0530

    added room joining logic for the ws-layer

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,10 +1,33 @@
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common";
+import { prismaClient } from "@repo/db";
 
 const wss = new WebSocketServer({
     port: 8080
 })
+
+interface User {
+    ws: WebSocket,
+    rooms: string[],
+    userId: string
+}
+
+const users: User[] = [];
+
+function checkUser(token: string) : string | null {
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if (typeof decoded == "string"){
+        return null;
+    }
+
+    if(!decoded || !(decoded as JwtPayload).userId){
+        return null;
+    }
+    
+    return decoded.userId;
+}
 
 wss.on('connection', function connection(ws, request){
     ws.on('error', console.error);
@@ -16,19 +39,42 @@ wss.on('connection', function connection(ws, request){
 
     const queryParams = new URLSearchParams(url.split('?')[1]);
     const token = queryParams.get('token') || "";
-    const decoded = jwt.verify(token, JWT_SECRET);
-
-    if (typeof decoded == "string"){
-        ws.close();
-        return;
+    const userId = checkUser(token);
+    
+    if(userId == null){
+        ws.close()
+        return null;
     }
 
-    if(!decoded || !(decoded as JwtPayload).userId){
-        ws.close();
-        return;
-    }
+    users.push({
+        userId,
+        rooms: [],
+        ws
+    })
 
-    ws.on('message', function message(data){
-        ws.send('pong');
+    ws.on('message',async function message(data){
+        const parsedData = JSON.parse(data as unknown as string); // {type: "join_room", roomId: 1}
+
+        const response = await prismaClient.room.findFirst({
+            where: {
+                id: parsedData.roomId
+            }
+        })
+        if(!response?.id){
+            return;
+        }
+
+        if (parsedData.type === "join_room"){
+            const user = users.find(x => x.ws === ws);
+            user?.rooms.push(parsedData.roomId);
+        }
+
+        if (parsedData.type === "leave_room"){
+            const user = users.find(x => x.ws === ws);
+            if (!user){
+                return;
+            }
+            user.rooms = user?.rooms.filter(x => x === parsedData.room);
+        }
     })
 })
